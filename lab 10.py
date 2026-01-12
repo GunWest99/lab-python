b9c4ac115b6fc99103db57b36aaa77461628ff7d
kirill <kirillstasevic10@gmail.com>
2026-01-12 17:15:11 +0300

    10

---
+++ lab 10.py
@@ -0,0 +1,61 @@
+# i=0
+# while i <10:
+#     print('Привет')
+#     i +=1
+#
+#
+#
+# for i in range (10):
+#     print('Привет')
+#
+#
+#
+# num=int(input())
+# while num !=-1:
+#     print('Квадрат вашего числа равен:',num*num)
+#     num=int(input())
+#
+#
+#
+#
+# text=input()
+# total=0
+# while text !='stop':
+#     num=int(text)
+#     total+=num
+#     text=input()
+# print('Сумма чисел равна',total)
+#
+#
+#
+# a=input()
+# d=0
+# while a!='Конец':
+#     a=input()
+#     if a!='Конец':
+#         print(a)
+#
+#
+#
+# a=input()
+# while a!='конец' and a!='КОНЕЦ':
+#     print(a)
+#     a=input()
+#
+#
+#
+#
+# a=input()
+# b=0
+# while a!='стоп'and a!='хватит'and a!= 'достаточно':
+#     print(b)
+
+
+
+a=int(input())
+b=0
+while a!=0:
+    if a>=25:
+    a%25
+
+    
