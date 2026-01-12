5cdcaeaf0d46736d839d3876bb7c6ac4266aa616
kirill <kirillstasevic10@gmail.com>
2026-01-12 17:16:03 +0300

    14

---
+++ lab 14.py
@@ -0,0 +1,121 @@
+# s='abcdefg'
+# print(s[0]+s[2]+s[4]+s[6])
+#
+#
+#
+# s='abcdefg'
+# print(s[0]*3+s[-1]*3+s[3]*2+s[3]*2)
+#
+#
+# s='abcdef'
+# for i in range(len(s)):
+#     print(s[i])
+#
+#
+#
+#
+#
+# s=input()
+# for i in range(0, len(s), 2):
+#     print(s[i])
+#
+#
+#
+#
+# s=input()
+# for i in range(len(s)+1):
+#     print(s[i])
+#
+#
+#
+#
+# a=input()
+# b=input()
+# c=input()
+# s=b,c,a
+# for i in range(0, len(s), 1):
+#    print(s[i])
+#
+#
+#
+#
+# s=input()
+# for c in s:
+#     print(c)
+#
+#
+#
+#
+# a=input()
+# for i in range (len(a)):
+#     if a[i] in '123456789':
+#         print('da')
+#         break
+# else:
+#     print('net')
+#
+#
+#
+#
+# a=input()
+# for c in a:
+#     if b[a]=='*':
+#         print('a')
+#
+#
+#
+#
+#
+# s='abcdefghi'
+# print(s[-9:-4])
+# print(s[-3:])
+# print(s[:-3])
+#
+#
+#
+# s='abcdefghi'
+# print(s[1:7:2])
+#
+#
+# s='abcdefghi'
+# print(s[::-1])
+# print(s[::-2])
+#
+#
+#
+#
+#
+# s='abcdefghi'
+# print(s[::-1])
+#
+#
+#
+# a=input()
+# b=a[:]
+# c=a[::-1]
+# if b==c:
+#     print('yes')
+# else:
+#     print('no')
+#
+#
+#
+#
+#
+#
+#
+# a=input()
+# for i in range(0, len [a+1]):
+#     print(i)
+#
+#
+#
+# s=' foo foo qux   '
+# print(s.strip())
+# print(s.lstrip())
+# print(s.rstrip())
+
+
+
+s='foo bar foo baz foo quz'
+print(s.replace('foo,'))
