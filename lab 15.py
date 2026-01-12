a3ec78f5b22a2851d453bb84f00ec8c694c9e833
kirill <kirillstasevic10@gmail.com>
2026-01-12 17:16:22 +0300

    15

---
+++ lab 15.py
@@ -0,0 +1,220 @@
+# s=input()
+# if s==s.title():
+#     print('Yes')
+# else:
+#     print('No')
+#
+#
+#
+#
+# a=input()
+# if 'хорош' in a.lower():
+#     print('Yes')
+# else:
+#     print('No')
+#
+#
+#
+#
+# s=input()
+# print(s.swapcase())
+#
+#
+#
+# s=input()
+# print(s.count(' ')+1)
+#
+#
+#
+#
+# a=input()
+# b=0
+# for i in range(10):
+#     b+=a.count((1))
+# print(b)
+#
+#
+#
+#
+# s=input()
+# if (s.find('com')or ('ru')):
+#     print(a)
+#
+#
+#
+#
+# a=input()
+# b=0
+# for i in range(len(a)):
+#     if a[i]=='f':
+#         print
+#
+#
+#
+#
+#
+# mylist=[]          # пустой список
+# mylist=list()      # пустой список
+#
+#
+#
+#
+# numbers=[2,4,6,8,10]
+# languages=['Python','C#','C++','Java']
+# print(numbers)
+# print(languages)
+#
+#
+#
+#
+# numbers=list(range(5))
+# even_numbers=list(range(0,10,2))
+# chars=list('abcde')
+#
+#
+#
+#
+#
+# numbers=[2,4,6,8,10]
+# if 2 in numbers:
+#     print('spisok da')
+# else:
+#     print('nety')
+#
+#
+#
+#
+#
+#
+# fruits=['apple','apricot','banana','cherry','kiwi','lemon','mango']
+# fruits=[2]='orange'
+# fruits=[-1]='peach'
+#
+#
+#
+#
+# print([1,2,3,4]+[5,6,7,8])
+# print([7,8]*3)
+# print([0]*10)
+#
+#
+#
+#
+#
+#
+# numbers=[1,2,3,4,5,6,7,8,9,10]
+# print(' Сумма всех элементов списка=',sum(numbers))
+#
+#
+#
+#
+# numbers=['8','9','10','11','12']
+# names=['Monica','Joey','Gunther','Chandler']
+# print('minimum=',min(numbers))
+# print('maximum=',max(names))
+#
+#
+#
+#
+#
+# books=['1984','0 дивный мир','451 градус по Фаренгейту']
+# print(books[0][1])
+# print(books[2][4:10])
+#
+#
+#
+#
+#
+# numbers=[1,1,2,3,5,8,13]
+# numbers.append(21)
+# numbers.append(34)
+# print(numbers)
+#
+#
+#
+#
+#
+# numbers=[]
+# numbers.append(1)
+# numbers.append(2)
+# numbers.append(3)
+# print(numbers)
+#
+#
+#
+#
+# numbers=[0,2,4,6,8,10]
+# odds=[1,3,5,7]
+# numbers.extend(odds)
+# print(numbers)
+#
+#
+#
+#
+#
+# a=[2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71]
+# print(a[16])
+# print(a[-1])
+# print(a[:6])
+# print(min(a))
+# print(max(a))
+#
+#
+#
+#
+# a=[2,4,6,8,10,12,14,16,18,20]
+# b=2+4+6+8+10+12+14+16+18+20
+# c=b//10
+# print(c)
+#
+#
+#
+#
+#
+# a=['Red','Orange','Yellow','Green','Blue','Indigo','Voilet']
+# print('Red','Orange','Yellow','Зелёный','Blue','Indigo','Фиолетовый')
+#
+#
+#
+#
+# languages=['Chinese','Spanish','English','Hindi','Arabic','Bengali','Portuguese','Russian','Japanese','Lahnda']
+# print('Lahnda','Japanese','Russian','Portuguese','Bengali','Arabic','Hindi','English','Spanish','Chinese')
+#
+#
+#
+#
+# a=input()
+# b=input()
+# c=input()
+# d=input()
+# e=input()
+# f=input()
+# g=input()
+# print(b,c,d,e,f,g)
+#
+#
+#
+#
+#
+# a=int(input())
+# b=int(input())
+# c=int(input())
+# d=int(input())
+# e=int(input())
+# f=int(input())
+# g=b**3
+# u=c**3
+# l=d**3
+# k=e**3
+# w=f**3
+# print(g,u,l,k,w)
+
+
+
+
+a=int(input())
+c=[]
+for i in range(a):
+    b=int(input())
+    c.append(b)
+print(c)
