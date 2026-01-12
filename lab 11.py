c17f99dfdab1c486e9970cea216aecf081f85293
kirill <kirillstasevic10@gmail.com>
2026-01-12 17:15:22 +0300

    11

---
+++ lab 11.py
@@ -0,0 +1,43 @@
+# n=int(input())
+# while n != 0:
+#     last_digit=n%10
+#     n=n//10
+#
+#
+#
+#
+# a=int(input())
+# if a==7:
+#     print('Yes')
+# if a!=7:
+#     print('No')
+#
+#
+#
+#
+# num=12345product=1
+# while num !=0:
+#     last_digit=num%10
+#     product=product*last_digit
+#     num=num//10
+# print(product)
+#
+#
+#
+#
+#
+# a=int(input())
+# while a!=0:
+#     b=a%10
+#     print(b)
+#     a=a//10
+
+
+
+
+a=int(input())
+while a !=0
+    b=a%10
+    c=b%10
+    d=b+c
+    print(d)
