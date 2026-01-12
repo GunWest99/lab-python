53dd95951e169be09df581a8b23f1d2a98af3079
kirill <kirillstasevic10@gmail.com>
2026-01-12 17:14:51 +0300

    8

---
+++ lab 8.py
@@ -0,0 +1,103 @@
+# for i in range (10):
+#     print('Привет')
+#
+#
+#
+# for i in range (5):
+#     num=int(input())
+#     print('Квадрат вашего числа равен;',num*num)
+# print('Цикл завершён')
+#
+#
+#
+#
+# for i in range (10):
+#     print(i)
+#
+#
+#
+#
+# for i in range (10):
+#     print('Python is awesome')
+#
+#
+#
+#
+# for i in range (5):
+#     print('AAA')
+# for i in range (5):
+#     print('BBBB')
+# print('E')
+# for i in range (6):
+#     print('TTTTT')
+# print('E')
+#
+#
+#
+# a=int(input())
+# for i in range (a):
+#     print('Век живи-век учись')
+#
+#
+#
+#
+# n=int(input())
+# for i in range (n):
+#     print('*' * 19)
+#
+#
+#
+# for i in range(10):
+#     print('i')
+# for i in range(1,10):
+#     print('i')
+# for i in range(3,7):
+#     print('i')
+# for i in range(7,3):
+#     print('i')
+# for i in range(2,15,3):
+#     print('i')
+# for i in range(9,2,-1):
+#     print('i')
+# for i in range(3,10,-2):
+#     print('i')
+#
+#
+#
+#
+# m=int(input())
+# n=int(input())
+# if m<=n:
+#     for i in range (m, n+1):
+#         print(i)
+#
+#
+#
+#
+# m=int(input())
+# n=int(input())
+# if m<n:
+#     for i in range(m, n + 1):
+#         print(i)
+# else :
+#     for i in range(m, n - 1, -1):
+#         print(i)
+#
+#
+#
+#
+# m=int(input())
+# n=int(input())
+# if m>=n:
+#     for i in range(m,n,-2):
+#         print(i)
+# else:
+#     for i in range(m,n,+2):
+#         print(i)
+
+
+
+m=int(input())
+n=int(input())
+for i in range(11):
+    print(m*n=x)
