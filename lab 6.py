197431a58c69776bbf67975690191e77d317dc55
kirill <kirillstasevic10@gmail.com>
2026-01-12 17:14:20 +0300

    6

---
+++ lab 6.py
@@ -0,0 +1,99 @@
+# grade=int(input())
+# if grade >=90:
+#     print(5)
+# else:
+#     if grade >= 80:
+#         print(4)
+#     else:
+#         if grade >= 70:
+#             print(3)
+#         else:
+#             if grade >= 60:
+#                 print(2)
+#             else:
+#                 print(1)
+#
+#
+#
+# grade=int(input())
+# if grade >=90:
+#     print(5)
+# elif grade >=80:
+#     print(4)
+# elif grade >= 70:
+#     print(3)
+# elif grade >= 60:
+#     print(2)
+# else:
+#     print(1)
+#
+#
+# a =input()
+# b =input()
+# c =input()
+# if a==b==c:
+#     print(3)
+# elif a==b:
+#     print(2)
+# elif a==c:
+#     print(2)
+# elif b==c:
+#     print(2)
+# else:
+#     print(0)
+#
+#
+#
+# a = input()
+# b = input()
+# c = input()
+# if a>b and a<c or a<b and a>c:
+#     print(a)
+# if b>a and b<c or b<a and b>c:
+#     print(b)
+# if c>a and c<b or c<b and c>a:
+#     print(c)
+#
+#
+#
+#
+# a =int(input())
+# if a<60:
+#     print('Лёгкий вес')
+# if a<60 and a<64:
+#     print('Первый полусредний вес')
+# if a>64 and a<69:
+#     print('Полусредний  вес')
+#
+#
+#
+# a=input()
+# b=input()
+# if a==('красный') and b==('синий'):
+#     print('Фиолетовый')
+# if a==('красный') and b==('зелёный'):
+#     print('жёлтый')
+# if a==('зелёный') and b==('синий'):
+#     print('бирюзовый')
+# else:
+#     print('Ошибка')
+
+
+
+
+
+
+print('Больше всего мне понравилось')
+a=input()
+print('Я почти переварил')
+a=input()
+print('Мне не понравилось')
+a=input()
+print('Я рекомендую')
+a=input()
+print('Я переел')
+a=input()
+print('Я съел бы ещё этого')
+a=input()
+print('Сейчас я себя чувствую')
+a=input() 
