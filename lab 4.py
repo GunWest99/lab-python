144fe580051412ff2c85819df6be02e529ade700
kirill <kirillstasevic10@gmail.com>
2026-01-12 17:13:36 +0300

    4

---
+++ lab 4.py
@@ -0,0 +1,36 @@
+# print ('Какой язык программирования мы изучаем?')
+# answer = input()
+# if answer == 'Python':
+#     print('да')
+# else:
+#     print('Нет !')
+#
+#
+#
+# num1=int(input())
+# num2=int(input())
+# if num1<num2:
+#     print (num1, 'меньше чем', num2)
+# if num1>num2:
+#     print(num1, 'больше чем', num2)
+# if num1==num2:
+#     print (num1, 'равно', num2)
+# if num1!= num2:
+#     print(num1, 'не равно', num2)
+
+
+a=(input())
+b=(input())
+c=(input())
+d=(input())
+if a<13:
+    print('детство')
+if b>14 and b<24 :
+    print('молодость')
+if с>25andс<59:
+    print('молодость')
+
+
+
+
+
