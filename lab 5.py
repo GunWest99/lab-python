84aee65cea46ed6e9f430b1f72050fcb349e04cc
kirill <kirillstasevic10@gmail.com>
2026-01-12 17:13:53 +0300

    5

---
+++ lab 5.py
@@ -0,0 +1,94 @@
+# age=int (input())
+# grade=int (input())
+# if age >=12 and grade >= 7:
+#     print('Доступ разрешён.')
+# else:
+#     print('Доступ запрещён.')
+
+
+# age=int(input())
+# grade=int(input())
+# city=input()
+# if age >=12 and grade >= 7 and city=='Москва':
+#     print('Доступ разрешён.')
+# else:
+#     print('Доступ запрещён.')
+
+
+#city=input()
+#if city == 'Москва' or city=='Уфа':
+#    print('Доступ разрешён.')
+#else:
+#    print('Доступ запрещён.')
+
+
+
+
+# age=int (input())
+# grade=int (input())
+# city=input()
+# if age >=12 and grade >= 7 and (city=='Москва' or
+#                            city=='Уфа' or city=='Тула'):
+#     print('Доступ разрешён.')
+#  else:
+#     print('Доступ запрещён.')
+#
+#
+#
+# age=int(input())
+# if not (age < 12):
+#     print('Доступ разрешён.')
+# else:
+#     print('Доступ запрещён.')
+#
+#
+#
+# age=int(input())
+# if age >=12:
+#     print('Доступ разрешён.')
+# else:
+#     print('Доступ запрещён.')
+#
+#
+#
+# a=int(input())
+# b=a%10
+# c=(a//10)%10
+# d=a//100
+# if b==c and c==d and d==b:
+#     print('Все цифры равны .')
+# else:
+#     print('цифры разные .')
+#
+#
+#
+#
+#
+# a=int(input())
+# b=int(input())
+# if a > 0 and b > 0:
+#     print('1 четверть')
+# if a < 0 and b > 0:
+#     print('2 четверть')
+# if a > 0 and b < 0:
+#     print('3 четверть')
+# if a < 0 and b < 0:
+#     print('4 четверть')
+#
+#
+#
+# a=int(input())
+# if a > -30 and a < -2:
+#     print('yes')
+# if a > 7 and a < 25:
+#     print('no')
+
+
+
+
+a=int(input())
+b=a//4
+if b==0:
+    print('высокосный')
+else:
+    print('невысокосный')
