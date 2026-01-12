3ea138de14faee6523fb62ccf1922bf7b9d4e073
kirill <kirillstasevic10@gmail.com>
2026-01-12 17:14:34 +0300

    7

---
+++ lab 7.py
@@ -0,0 +1,108 @@
+# num1=int('1234')
+# num2=int('4321')
+# print(num1+num2)
+#
+#
+#
+# num1=int('123')
+# num2=int('2!')
+# print(num1+num2)
+#
+#
+#
+# num1=int('3')
+# num2=int('2.5')
+# print(num1-num2)
+#
+# num1=int(1.5)
+# num2=int(6)
+# print(num1+num2)
+#
+#
+#
+# num1=float('-0.5')
+# num1=float('6')
+# print(num1+num2)
+#
+#
+# num1=float('2.0')
+# num1=float('5+5')
+# print(num1-num2)
+#
+#
+#
+# num1=float(3.0)
+# num2=float(2-2)
+# print(num1*num2)
+#
+#
+#
+# num1=float(input())
+# num2=float(input())
+# S=num1*num2*0.5
+# print(S)
+#
+#
+#
+#
+# a1=float(input())
+# a2=(9/5)*(a1+32)
+# print(a2)
+#
+#
+#
+#
+# x=float(input())
+# print(int(x*10)%10)
+#
+#
+#
+# num= max (1,3,-5,7) + min(-3,6,-8,-1)+abs(-17)
+# print(num)
+#
+#
+#
+# a=int(input())
+# b=int(input())
+# c=int(input())
+# d=max(a,b,c)
+# e=min(a,b,c)
+# f=abs((a+b+c)-d-e)
+# print(d,f,e)
+#
+#
+#
+#
+# a=float(input())
+# b=float(input())
+# c=float(input())
+# d=float(input())
+# e=float(input())
+# h=abs(a)+abs(b)+abs(c)+abs(d)+abs(e)
+# print(h)
+#
+#
+#
+# str1 = '1'
+# str2=str1+'3'+str1
+# str3=str2+'3'+str2
+# str4=str3+'4'+str3
+# print(str4)
+#
+#
+#
+# str1=input()
+# str2=str1
+# print('Футбольная команда Barcelona  имеет длинну 9 символов' )
+#
+#
+#
+#a=input()
+#b=input()
+#c=input()
+#d=max(a,b,c)
+#e=min(a,b,c)
+#print(d,e)
+
+
+
