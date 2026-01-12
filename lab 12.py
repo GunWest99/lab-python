2f7f1dc4fb2ef9f2ebf611a760c2d4721c2a3850
kirill <kirillstasevic10@gmail.com>
2026-01-12 17:15:34 +0300

    12

---
+++ lab 12.py
@@ -0,0 +1,90 @@
+# result=0
+# for i in range (10):
+#     num=int(input())
+#     if num < 0:
+#         break
+#     result+=num
+# print(result)
+#
+#
+#
+# num=int(input())
+# flag=True
+#
+# for i in range(2,num):
+#     if num % i ==0:
+#         flag=False
+#         break
+# if flag==True:
+#     print('Число простое')
+# else:
+#     print('Число составное')
+#
+#
+#
+# n=int(input())
+# flag=False
+# while n !=0:
+#     last_digit = n % 10:
+#     if last_digit==7:
+#         flag=True
+#         break
+#     n //=10
+# if flag
+#
+#
+#
+# while True:
+#     print('Python awesome!')
+#
+#
+#
+#
+#
+# for i in range (10):
+#     print(i, end='*')
+#     if i >6:
+#         break
+#
+#
+#
+# mult=1
+# for i in range (1,11):
+#     if i % 2  ==0:
+#         continue
+#     if i % 9 ==0:
+#         break
+#     mult *=i
+# print(mult)
+#
+#
+#
+# i=1
+# while i <101:
+#     if i % 7==0:
+#         print(i)
+#     i +=1
+#
+#
+#
+# for i in range(7,101,7):
+#     print(i)
+#
+#
+#
+#
+# for i in range (100, 0, -1):
+#     print(i)
+#
+#
+#
+# a=0
+# for i in range(1,1001):
+#     if i % 2 ==1:
+#         a+=i
+# print(a)
+#while num !=
+
+
+
+
